15b64b7891861c3e49801c21ec3fca96480697d0
Daniel Cousens <github@dcousens.com>
2015-08-10 10:19:00 +1000

    better benchmark suite

--- bench/index.js
+++ bench/index.js
@@ -1,36 +1,37 @@
 var assert = require('assert')
 var benchmark = require('benchmark')
+benchmark.options.minTime = 1
 
 var local = require('../')
 var npm = require('typeforce')
 
-var fixtures = require('../test/fixtures')
+// var fixtures = require('../test/fixtures')
+var fixtures = [
+  { 'type': '?Number', 'value': null },
+  { 'type': [ '?Number' ], 'value': [ 1, null ] },
+  { 'type': [ { 'a': 'Number' } ], 'value': [ { 'a': 1 }, { 'a': 2 } ] },
+  { 'type': [ { 'a': '?Number' } ], 'value': [ { 'a': 1 }, { 'a': null } ] }
+]
 
-function CustomType () { return 'ensure non-greedy match'.toUpperCase() }
-var CUSTOM_TYPES = {
-  'Buffer': new Buffer(1),
-  'CustomType': new CustomType(),
-  'Function': function () {}
-}
-
-fixtures.valid.forEach(function (f) {
-  var actualValue = f.custom ? CUSTOM_TYPES[f.custom] : f.value
+fixtures.forEach(function (f) {
   var suite = new benchmark.Suite()
   var ctype = local.compile(f.type)
 
   if (f.exception) {
-    assert.throws(function () { local(ctype, actualValue, f.strict) }, new RegExp(f.exception))
-    assert.throws(function () { npm(ctype, actualValue, f.strict) }, new RegExp(f.exception))
+    assert.throws(function () { local(ctype, f.value, f.strict) }, new RegExp(f.exception))
+    assert.throws(function () { npm(ctype, f.value, f.strict) }, new RegExp(f.exception))
 
-    suite.add('local(e)#' + f.type, function () { try { local(ctype, actualValue, f.strict) } catch (e) {} })
-    suite.add('  npm(e)#' + f.type, function () { try { npm(ctype, actualValue, f.strict) } catch (e) {} })
+    suite.add('local(e)#' + f.type, function () { try { local(ctype, f.value, f.strict) } catch (e) {} })
+    suite.add('  npm(e)#' + f.type, function () { try { npm(ctype, f.value, f.strict) } catch (e) {} })
 
   } else {
-    local(ctype, actualValue, f.strict)
-    npm(ctype, actualValue, f.strict)
+    local(ctype, f.value, f.strict)
+    npm(ctype, f.value, f.strict)
 
-    suite.add('local#' + f.type, function () { local(ctype, actualValue, f.strict) })
-    suite.add('  npm#' + f.type, function () { npm(ctype, actualValue, f.strict) })
+    suite.add('local#' + f.type, function () { local(f.type, f.value, f.strict) })
+    suite.add('  npm#' + f.type, function () { npm(f.type, f.value, f.strict) })
+    suite.add('local#(c)' + f.type, function () { local(ctype, f.value, f.strict) })
+    suite.add('  npm#(c)' + f.type, function () { npm(ctype, f.value, f.strict) })
   }
 
   // after each cycle
