7bc9dcc0e57d23d806acf4193e6c05953101c50a
Daniel Cousens <github@dcousens.com>
2015-09-09 19:52:34 +1000

    tests: simplify type descriptions

--- test/types.js
+++ test/types.js
@@ -3,21 +3,21 @@ var typeforce = require('../src')
 function Tt () { return false }
 
 var TYPES = {
-  '"(Boolean, Number)"': typeforce.tuple('Boolean', 'Number'),
-  '"(Number|String)"': typeforce.tuple(typeforce.oneOf('Number', 'String')),
-  '"(Number)"': typeforce.tuple('Number'),
-  '["?{\\"a\\":\\"Number\\"}"]': [ typeforce.maybe({ a: 'Number' }) ],
-  '"Boolean|Number|String"': typeforce.oneOf('Boolean', 'Number', 'String'),
-  '"?Boolean|Number"': typeforce.maybe(typeforce.oneOf('Boolean', 'Number')),
-  '"?{\\"a\\":\\"?Number\\"}"': typeforce.maybe({ a: '?Number' }),
-  '"?{\\"a\\":\\"Number\\"}"': typeforce.maybe({ a: 'Number' }),
-  '{"a":"Number|Null"}': { a: typeforce.oneOf('Number', 'Null') },
-  '{"a":"Number|{\\"b\\":\\"Number\\"}"}': { a: typeforce.oneOf('Number', { b: 'Number' }) },
-  '{"a":"?{\\"b\\":\\"Number\\"}"}': { a: typeforce.maybe({ b: 'Number' }) },
-  '{"a":"?{\\"b\\":\\"?{\\\\\\"c\\\\\\":\\\\\\"Number\\\\\\"}\\"}"}': { a: typeforce.maybe({ b: typeforce.maybe({ c: 'Number' }) }) },
+  '(Boolean, Number)': typeforce.tuple('Boolean', 'Number'),
+  '(Number|String)': typeforce.tuple(typeforce.oneOf('Number', 'String')),
+  '(Number)': typeforce.tuple('Number'),
+  '[?{ a: Number }]': [ typeforce.maybe({ a: 'Number' }) ],
+  'Boolean|Number|String': typeforce.oneOf('Boolean', 'Number', 'String'),
+  '?Boolean|Number': typeforce.maybe(typeforce.oneOf('Boolean', 'Number')),
+  '?{ a: ?Number }': typeforce.maybe({ a: '?Number' }),
+  '?{ a: Number }': typeforce.maybe({ a: 'Number' }),
+  '{ a: Number|Null }': { a: typeforce.oneOf('Number', 'Null') },
+  '{ a: Number|{ b: Number } }': { a: typeforce.oneOf('Number', { b: 'Number' }) },
+  '{ a: ?{ b: Number } }': { a: typeforce.maybe({ b: 'Number' }) },
+  '{ a: ?{ b: ?{ c: Number } } }': { a: typeforce.maybe({ b: typeforce.maybe({ c: 'Number' }) }) },
   '?Tt': Tt,
-  '{"a":"?Tt"}': { a: typeforce.maybe(Tt) },
-  '{"a":"{\\"b\\":\\"Tt\\"}"}': { a: { b: Tt } }
+  '{ a: ?Tt }': { a: typeforce.maybe(Tt) },
+  '{ a: { b: Tt } }': { a: { b: Tt } }
 }
 
 var VALUES = {
