81372a2dd3f220935af6275d6b481d9566f9901a
Daniel Cousens <github@dcousens.com>
2016-09-30 18:35:36 +1000

    tests: add Buffer3

--- test/values.js
+++ test/values.js
@@ -2,5 +2,6 @@ module.exports = {
   'function': function () {},
   'emptyType': new function EmptyType () {}(),
   'customType': new function CustomType () { this.x = 2 }(),
-  'buffer': new Buffer(0)
+  'buffer': new Buffer(0),
+  'buffer3': new Buffer('ffffff', 'hex')
 }
