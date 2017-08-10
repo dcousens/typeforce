363c230ff3aaab19ed37042bdc04ea46914df245
Daniel Cousens <github@dcousens.com>
2017-08-10 13:36:02 +1000

    bench: test exceptions too

--- bench/index.js
+++ bench/index.js
@@ -4,15 +4,25 @@ const local = require('../')
 const npm = require('typeforce')
 const TYPES = require('../test/types')
 const VALUES = require('../test/values')
-const fixtures = require('../test/fixtures').valid
+const tests = require('../test/fixtures')
+const fixtures = tests.valid.concat(tests.invalid)
 
 fixtures.forEach(function (f) {
   const type = TYPES[f.typeId] || f.type
   const value = VALUES[f.valueId] || f.value
   const ctype = local.compile(type)
 
-  local(ctype, value, f.strict)
-  npm(ctype, value, f.strict)
+  if (f.exception) {
+    assert.throws(function () { local(type, value, f.strict) }, new RegExp(f.exception))
+//      assert.throws(function () { npm(type, value, f.strict) }, new RegExp(f.exception))
+    assert.throws(function () { local(ctype, value, f.strict) }, new RegExp(f.exception))
+//      assert.throws(function () { npm(ctype, value, f.strict) }, new RegExp(f.exception))
+  } else {
+    local(type, value, f.strict)
+    npm(type, value, f.strict)
+    local(ctype, value, f.strict)
+    npm(ctype, value, f.strict)
+  }
 })
 
 // benchmark.options.minTime = 1
@@ -24,18 +34,13 @@ fixtures.forEach(function (f) {
   const ctype = local.compile(type)
 
   if (f.exception) {
-    assert.throws(function () { local(f.type, value, f.strict) }, new RegExp(f.exception))
-    assert.throws(function () { npm(f.type, value, f.strict) }, new RegExp(f.exception))
-    assert.throws(function () { local(ctype, value, f.strict) }, new RegExp(f.exception))
-    assert.throws(function () { npm(ctype, value, f.strict) }, new RegExp(f.exception))
-
-    suite.add('local(e)#' + tdescription, function () { try { local(f.type, value, f.strict) } catch (e) {} })
-    suite.add('  npm(e)#' + tdescription, function () { try { npm(f.type, value, f.strict) } catch (e) {} })
+    suite.add('local(e)#' + tdescription, function () { try { local(type, value, f.strict) } catch (e) {} })
+    suite.add('  npm(e)#' + tdescription, function () { try { npm(type, value, f.strict) } catch (e) {} })
     suite.add('local(c, e)#' + tdescription, function () { try { local(ctype, value, f.strict) } catch (e) {} })
     suite.add('  npm(c, e)#' + tdescription, function () { try { npm(ctype, value, f.strict) } catch (e) {} })
   } else {
-    suite.add('local#' + tdescription, function () { local(f.type, value, f.strict) })
-    suite.add('  npm#' + tdescription, function () { npm(f.type, value, f.strict) })
+    suite.add('local#' + tdescription, function () { local(type, value, f.strict) })
+    suite.add('  npm#' + tdescription, function () { npm(type, value, f.strict) })
     suite.add('local(c)#' + tdescription, function () { local(ctype, value, f.strict) })
     suite.add('  npm(c)#' + tdescription, function () { npm(ctype, value, f.strict) })
   }
